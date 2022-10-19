3ee783a84f6db6996d2a8ccbdeb63b45c794711e
Tri Denda <tridenda.nk@gmail.com>
2022-10-19 17:13:11 +0700

    build: commented all code in twin.d.ts

--- src/types/twin.d.ts
+++ src/types/twin.d.ts
@@ -1,32 +1,31 @@
-// types/twin.d.ts
-import styledImport, { css as cssImport, CSSProp } from "styled-components";
-import "twin.macro";
+// import styledImport, { css as cssImport, CSSProp } from "styled-components";
+// import "twin.macro";
 
-declare module "twin.macro" {
-  // The styled and css imports
-  const styled: typeof styledImport;
-  const css: typeof cssImport;
-}
+// declare module "twin.macro" {
+//   // The styled and css imports
+//   const styled: typeof styledImport;
+//   const css: typeof cssImport;
+// }
 
-declare module "react" {
-  // The css prop
-  type HTMLAttributes<T> = {
-    css?: CSSProp;
-    tw?: string;
-  } & DOMAttributes<T>;
+// declare module "react" {
+//   // The css prop
+//   type HTMLAttributes<T> = {
+//     css?: CSSProp;
+//     tw?: string;
+//   } & DOMAttributes<T>;
 
-  // The inline svg css prop
-  type SVGProps<T> = {
-    css?: CSSProp;
-    tw?: string;
-  } & SVGProps<SVGSVGElement>;
-}
+//   // The inline svg css prop
+//   type SVGProps<T> = {
+//     css?: CSSProp;
+//     tw?: string;
+//   } & SVGProps<SVGSVGElement>;
+// }
 
-// The 'as' prop on styled components
-declare global {
-  namespace JSX {
-    type IntrinsicAttributes<T> = {
-      as?: string | Element;
-    } & DOMAttributes<T>;
-  }
-}
+// // The 'as' prop on styled components
+// declare global {
+//   namespace JSX {
+//     type IntrinsicAttributes<T> = {
+//       as?: string | Element;
+//     } & DOMAttributes<T>;
+//   }
+// }
